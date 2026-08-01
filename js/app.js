588e0f7deddc530f7b50df85225448516893f787
CezarG-9 <cezar.gherghina9@gmail.com>
2026-08-01 21:29:18 +0100

    Initial project setup

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const statusElement = document.querySelector("#status");
+
+statusElement.textContent = "HTML, CSS and JavaScript are connected successfully.";
+
+console.log("Torn Job Finder JavaScript loaded.");
